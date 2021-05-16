badd3f6680c9342b13cf9f9fa5f36ec26545324e
Nelkor <nelkor9855118751@gmail.com>
2021-05-16 17:05:30 +0300

    draw cells

--- src/modules/render/renderer.ts
+++ src/modules/render/renderer.ts
@@ -1,9 +1,17 @@
-import { side, ctx } from './canvas'
+import { side, cellSide, ctx } from './canvas'
 
 const draw = () => {
   requestAnimationFrame(draw)
 
-  ctx.clearRect(0, 0, side, side)
+  ctx.fillStyle = '#776666'
+  ctx.fillRect(0, 0, side, side)
+  ctx.fillStyle = '#eedddd'
+
+  for (let i = 0; i < 8; i++) {
+    for (let j = i % 2; j < 8; j += 2) {
+      ctx.fillRect(j * cellSide, i * cellSide, cellSide, cellSide)
+    }
+  }
 }
 
 export const startRender = (): void => {
