c73349e45e525c20c3ad9b95947a86065b2cd823
Nelkor <nelkor9855118751@gmail.com>
2021-05-16 17:05:30 +0300

    draw cells

--- src/modules/render/canvas.ts
+++ src/modules/render/canvas.ts
@@ -3,12 +3,14 @@ import { createCtx } from 'ctx-2d'
 const parent = document.querySelector('.canvas-wrapper')
 
 export let side = 0
+export let cellSide = 0
 
 export const ctx = createCtx(parent, {
   resizeCallback(ctx) {
     const { width, height } = ctx.canvas
 
     side = Math.min(width, height)
+    cellSide = side / 8
   },
   limits: {
     width: 1024,
